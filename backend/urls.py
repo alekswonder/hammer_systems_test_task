cbfe0491c00a4fdab82e29c975f179b1fd209f64
Alexander Nepein <alekswonder@yandex.ru>
2023-08-20 21:36:31 +0500

    Add docker

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.urls import include, path
 
-# from django.views.generic import TemplateView
+from django.urls import re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API для авторизации",
+        default_version='v1',
+        description="Документация для авторизации по токену и SMS-коду",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    # path('redoc/',
-    #      TemplateView.as_view(template_name='redoc.html'),
-    #      name='redoc')
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
 ]
